c900277b3465093a7b749f6b841dfec2db2809f6
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2024-12-24 20:20:32 -0800

    fixed termination bug, added extra logging

--- clicker.py
+++ clicker.py
@@ -3,30 +3,36 @@ import random
 import time
 import threading
 import pygetwindow as gw
+from datetime import datetime
 
 active = False
 target_window = "Fortnite  "
 lock = threading.Lock()
+is_terminated = False
 
 # def window_res():s
 #     while True:
 #         print(gw.getActiveWindow())
 
+def log_with_time(message):
+    print(f"{datetime.now().strftime('[(%Y-%m-%d) %H:%M:%S]')} {message}")
 
 def auto_click():
-    while True:
+    global is_terminated
+    while not is_terminated:
         with lock:
             active_window = gw.getActiveWindow()
             if not active or active_window is None or target_window != active_window.title:
                 continue
             pyautogui.click()
-            print("click")
+            log_with_time("Click")
 
             delay = random.uniform(120, 260)
             time.sleep(delay)
 
 def auto_move():
-    while True:
+    global is_terminated
+    while not is_terminated:
         with lock:
             active_window = gw.getActiveWindow()
             if not active or active_window is None or target_window != active_window.title:
@@ -39,62 +45,59 @@ def auto_move():
                 pyautogui.keyDown("w")
                 time.sleep(move_time)
                 pyautogui.keyUp("w")
-                print("w")
+                log_with_time("w")
             elif movement == 2:
                 pyautogui.move
                 pyautogui.keyDown("a")
                 time.sleep(move_time)
                 pyautogui.keyUp("a")
-                print("a")
+                log_with_time("a")
             elif movement == 3:
                 pyautogui.keyDown("s")
                 time.sleep(move_time)
                 pyautogui.keyUp("s")
-                print("s")
+                log_with_time("s")
             elif movement == 4:
                 pyautogui.keyDown("d")
                 time.sleep(move_time)
                 pyautogui.keyUp("d")
-                print("d")
+                log_with_time("d")
             elif movement == 5:
                 pyautogui.keyDown("w")
                 pyautogui.keyDown("a")
                 time.sleep(move_time)
                 pyautogui.keyUp("w")
                 pyautogui.keyUp("a")
-                print("wa")
+                log_with_time("wa")
             elif movement == 6:
                 pyautogui.keyDown("w")
                 pyautogui.keyDown("d")
                 time.sleep(move_time)
                 pyautogui.keyUp("w")
                 pyautogui.keyUp("d")
-                print("wd")
+                log_with_time("wd")
             elif movement == 7:
                 pyautogui.keyDown("d")
                 pyautogui.keyDown("a")
                 time.sleep(move_time)
                 pyautogui.keyUp("d")
                 pyautogui.keyUp("a")
-                print("da")
+                log_with_time("da")
             elif movement == 8:
                 pyautogui.keyDown("s")
                 pyautogui.keyDown("a")
                 time.sleep(move_time)
                 pyautogui.keyUp("s")
                 pyautogui.keyUp("a")
-                print("sa")
+                log_with_time("sa")
 
             # list, jump table
-
-
             delay = random.uniform(30, 150)
             time.sleep(delay)
 
 def check_input():
-    is_checking = True
-    global active
-    while is_checking:
+    global active, is_terminated
+    while True:
         is_active = input()
         with lock:
             if is_active == "stop":
@@ -104,8 +107,10 @@ def check_input():
                 active = True
                 print("Key log:")
             elif is_active == "terminate":
-                is_checking = False
+                print("Terminating all threads")
                 is_active = False
+                is_terminated = True
+                break
 
 
 def main():
@@ -115,13 +120,16 @@ def main():
     thread1=threading.Thread(target=auto_click, daemon=True)
     thread2=threading.Thread(target=auto_move, daemon=True)
     # thread4=threading.Thread(target=window_res, daemon=True)
-    thread3=threading.Thread(target=check_input)
+
+    # thread3=threading.Thread(target=check_input)
     thread1.start()
     thread2.start()
-    thread3.start()
-    # thread4.start()
+    # thread3.start()
+    # # thread4.start()
 
-    thread3.join()
+    # thread3.join()
+
+    check_input()
 
     
 
