85f27aad0dd2dba77c24575c1ba9929ec6038926
rzhang57 <48890532+rzhang57@users.noreply.github.com>
2024-12-30 23:07:58 -0800

    added mouse movements

--- clicker.py
+++ clicker.py
@@ -37,9 +37,24 @@ def auto_move():
             active_window = gw.getActiveWindow()
             if not active or active_window is None or target_window != active_window.title:
                 continue
+
+            window_left = active_window.left
+            window_top = active_window.top
+            window_width = active_window.width
+            window_height = active_window.height
+
             movement = random.randint(1, 8)
             move_time = random.randint(6, 30)
             # move_time = random.randint(1, 2)
+            mouse_move_time = random.uniform(0.5, 2.0)
+
+            target_x = random.randint(window_left, window_left + window_width)
+            target_y = random.randint(window_top, window_top + window_height)
+
+            pyautogui.moveTo(target_x, target_y, duration=mouse_move_time)
+            log_with_time(f"Mouse moved to ({target_x}, {target_y})")
+
+            
 
             if movement == 1:
                 pyautogui.keyDown("w")
